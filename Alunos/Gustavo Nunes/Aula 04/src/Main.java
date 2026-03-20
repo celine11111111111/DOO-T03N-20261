107b5660d00a09aa3c304aaeb47b9701c3935aea
NUNES <nunesfariasgustavo@gmail.com>
2026-03-20 20:35:27 -0300

    First commit

---
+++ Alunos/Gustavo Nunes/Aula 04/src/Main.java
@@ -0,0 +1,13 @@
+import controller.VendaController;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Cria instancia do sistema
+        VendaController vendaController = new VendaController();
+
+        // inicia instancia
+        vendaController.iniciar();
+    }
+}
