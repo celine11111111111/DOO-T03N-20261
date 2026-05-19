df61c228e1576cccf40bc19f0e4d5fd8d7eb3da6
isabel <idgomes1@minha.fag.edu.br>
2026-05-18 21:37:07 -0300

    acho que finalizei

--- Alunos/Isabel Gomes/Aula09/CalculadoraComSwing/src/fag/Calculadora.java
+++ Alunos/Isabel Gomes/Aula09/CalculadoraComSwing/src/fag/Calculadora.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -28,7 +29,7 @@ public class Calculadora {
 		
 		//cria o visor da caluclçadora
 		panel.setLayout(null);
-		JTextField visor =  new JTextField("0");
+		JTextField visor =  new JTextField();
 	    visor.setBounds(0, 0, 350, 80);
 	    visor.setEditable(false);
 		panel.add(visor, BorderLayout.NORTH);	//coloca em cima o visor
@@ -39,6 +40,7 @@ public class Calculadora {
 		//botoes
 		//numeros
 		JButton b0 = new JButton("0");
+		b0.setBounds(90, 310, 60, 60);
 		b0.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -48,6 +50,7 @@ public class Calculadora {
 		panel.add(b0);
 		
 		JButton b1 = new JButton("1");
+		b1.setBounds(20, 240, 60, 60);
 		b1.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -57,6 +60,7 @@ public class Calculadora {
 		panel.add(b1);
 		
 		JButton b2 = new JButton("2");
+		b2.setBounds(90, 240, 60, 60);
 		b2.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -66,6 +70,7 @@ public class Calculadora {
 		panel.add(b2);
 		
 		JButton b3 = new JButton("3");
+		b3.setBounds(160, 240, 60, 60);
 		b3.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -75,6 +80,7 @@ public class Calculadora {
 		panel.add(b3);
 		
 		JButton b4 = new JButton("4");
+		b4.setBounds(20, 170, 60, 60);
 		b4.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -84,6 +90,7 @@ public class Calculadora {
 		panel.add(b4);
 		
 		JButton b5 = new JButton("5");
+		b5.setBounds(90, 170, 60, 60);
 		b5.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -93,6 +100,7 @@ public class Calculadora {
 		panel.add(b5);
 		
 		JButton b6 = new JButton("6");
+		b6.setBounds(160, 170, 60, 60);
 		b6.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -102,6 +110,7 @@ public class Calculadora {
 		panel.add(b6);
 		
 		JButton b7 = new JButton("7");
+		b7.setBounds(20, 100, 60, 60);
 		b7.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -111,6 +120,7 @@ public class Calculadora {
 		panel.add(b7);
 		
 		JButton b8 = new JButton("8");
+		b8.setBounds(90, 100, 60, 60);
 		b8.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -120,6 +130,7 @@ public class Calculadora {
 		panel.add(b8);
 		
 		JButton b9 = new JButton("9");
+		b9.setBounds(160, 100, 60, 60);
 		b9.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -131,6 +142,8 @@ public class Calculadora {
 		
 		//operacoes
 		JButton soma = new JButton("+");
+		soma.setBounds(240, 100, 60, 60);
+		soma.setBackground(Color.ORANGE);
 		soma.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -144,6 +157,8 @@ public class Calculadora {
 		panel.add(soma);
 		
 		JButton div = new JButton("/");
+		div.setBounds(240, 310, 60, 60);
+		div.setBackground(Color.ORANGE);
 		div.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -157,6 +172,8 @@ public class Calculadora {
 		panel.add(div);
 		
 		JButton mult = new JButton("*");
+		mult.setBounds(240, 240, 60, 60);
+		mult.setBackground(Color.ORANGE);
 		mult.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -169,6 +186,8 @@ public class Calculadora {
 		panel.add(mult);
 		
 		JButton menos = new JButton("-");
+		menos.setBounds(240, 170, 60, 60);
+		menos.setBackground(Color.ORANGE);
 		menos.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -182,7 +201,9 @@ public class Calculadora {
 		
 		
 		//outros dois botao
-		JButton limpar = new JButton("<-");
+		JButton limpar = new JButton("<--");
+		limpar.setBounds(160, 310, 60, 60);
+		limpar.setBackground(Color.ORANGE);
 		limpar.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -192,12 +213,56 @@ public class Calculadora {
 		panel.add(limpar);
 		
 		JButton igual = new JButton("=");
+		igual.setBounds(20, 310, 60, 60);
+		igual.setBackground(Color.ORANGE);
 		igual.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
+				try {
+					double numero2 = Double.parseDouble(visor.getText());
+					double resultado = 0;
+					
+					switch(operacao) {
+					
+					case "+":
+						resultado = numero1 + numero2;
+					break;
+					
+					case "-":
+						resultado = numero1 - numero2;
+					break;
+					
+					case "*":
+						resultado = numero1 * numero2;
+					break;
+					
+					case "/":
+						if(numero2 == 0 ) {
+							throw new DividirPorZeroException("Nao pode dividir por zero !!");
+						}
+						
+						resultado = numero1 / numero2;
+
+					break;
+					
+					}
+					
+					visor.setText(String.valueOf(resultado));
+					
+				}catch (DividirPorZeroException erro) {
+					
+					JOptionPane.showMessageDialog(frame, 
+					erro.getMessage(),
+					"Erro de divisao",
+					JOptionPane.ERROR_MESSAGE
+					);
+
+					//visor.setText(erro.getMessage());
+
+				}
 			}
 		});
+		
 		panel.add(igual);
 		
 		
